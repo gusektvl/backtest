f4a1ddd37441ade2c26760e1754b4bcf38cf64b5
J Park <sk46624191@gmail.com>
2022-12-09 16:04:55 +0900

    dx hourly trading backtest

---
+++ bt_dxy.py
@@ -0,0 +1,175 @@
+from backtesting.test import RSI, EMA, SMA
+from backtesting import Backtest, Strategy
+import yfinance as yf
+from datetime import datetime
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+
+ticker = '^KS11'
+today = datetime.today().strftime('%Y-%m-%d')
+start, end = '2022-06-01', '2022-12-09'
+
+DXY = yf.download(tickers=ticker, start=start, end=end, interval='1h',)
+data = DXY.copy()
+
+def BBANDS(data, n_lookback, n_std):
+    """Bollinger bands indicator"""
+    hlc3 = (data.High + data.Low + data.Close) / 3
+    mean, std = hlc3.rolling(n_lookback).mean(), hlc3.rolling(n_lookback).std()
+    upper = mean + n_std*std
+    lower = mean - n_std*std
+    return upper, lower
+
+close = data.Close.values
+MA = EMA
+ma10 = MA(data.Close, 10)
+ma20 = MA(data.Close, 20)
+ma50 = MA(data.Close, 50)
+ma100 = MA(data.Close, 100)
+upper, lower = BBANDS(data, 20, 2)
+
+# Design matrix / independent features:
+
+# Price-derived features
+data['X_MA10'] = (close - ma10) / close
+data['X_MA20'] = (close - ma20) / close
+data['X_MA50'] = (close - ma50) / close
+data['X_MA100'] = (close - ma100) / close
+
+data['X_DELTA_MA10'] = (ma10 - ma20) / close
+data['X_DELTA_MA20'] = (ma20 - ma50) / close
+data['X_DELTA_MA50'] = (ma50 - ma100) / close
+
+# Indicator features
+data['X_MOM'] = data.Close.pct_change(periods=2)
+data['X_BB_upper'] = (upper - close) / close
+data['X_BB_lower'] = (lower - close) / close
+data['X_BB_width'] = (upper - lower) / close
+data['X_Sentiment'] = ~data.index.to_series().between('2022-08-01', '2022-12-01')
+
+# Some datetime features for good measure
+data['X_day'] = data.index.dayofweek
+data['X_hour'] = data.index.hour
+
+data = data.dropna().astype(float)
+
+
+def get_X(data):
+    """Return model design matrix X"""
+    return data.filter(like='X').values
+
+def get_y(data):
+    """Return dependent variable y"""
+    y = data.Close.pct_change(48).shift(-48)  # Returns after roughly two days
+    y[y.between(-.004, .004)] = 0             # Devalue returns maller than 0.4%
+    y[y > 0] = 1
+    y[y < 0] = -1
+    return y
+
+def get_clean_Xy(df):
+    """Return (X, y) cleaned of NaN values"""
+    X = get_X(df)
+    y = get_y(df).values
+    isnan = np.isnan(y)
+    X = X[~isnan]
+    y = y[~isnan]
+    return X, y
+
+X, y = get_clean_Xy(data)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.7, random_state=0)
+
+clf = KNeighborsClassifier(7)  # Model the output based on 7 "nearest" examples
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+_ = pd.DataFrame({'y_true': y_test, 'y_pred': y_pred}).plot(figsize=(15, 2), alpha=.7)
+print('Classification accuracy: ', np.mean(y_test == y_pred))
+
+N_TRAIN = 400
+class MLTrainOnceStrategy(Strategy):
+    price_delta = .004  # 0.4%
+    n_neighbors = 5
+    def init(self):
+        self.rsi = self.I(RSI, data.Close, 14, plot=True, overlay=False)
+
+        # Init our model, a kNN classifier
+        self.clf = KNeighborsClassifier(self.n_neighbors)
+
+        # Train the classifier in advance on the first N_TRAIN examples
+        df = self.data.df.iloc[:N_TRAIN]
+        X, y = get_clean_Xy(df)
+        self.clf.fit(X, y)
+
+        # Plot y for inspection
+        self.I(get_y, self.data.df, name='y_true')
+
+        # Prepare empty, all-NaN forecast indicator
+        self.forecasts = self.I(lambda: np.repeat(np.nan, len(self.data)), name='forecast')
+
+    def next(self):
+        # Skip the training, in-sample data
+        if len(self.data) < N_TRAIN:
+            return
+
+        # Proceed only with out-of-sample data. Prepare some variables
+        high, low, close = self.data.High, self.data.Low, self.data.Close
+        current_time = self.data.index[-1]
+
+        # Forecast the next movement
+        X = get_X(self.data.df.iloc[-1:])
+        forecast = self.clf.predict(X)[0]
+
+        # Update the plotted "forecast" indicator
+        self.forecasts[-1] = forecast
+
+        # If our forecast is upwards and we don't already hold a long position
+        # place a long order for 20% of available account equity. Vice versa for short.
+        # Also set target take-profit and stop-loss prices to be one price_delta
+        # away from the current closing price.
+        upper, lower = close[-1] * (1 + np.r_[1, -1]*self.price_delta)
+
+        if forecast == 1 and not self.position.is_long:
+            self.buy(size=.2, tp=upper, sl=lower)
+        elif forecast == -1 and not self.position.is_short:
+            self.sell(size=.2, tp=lower, sl=upper)
+
+        # Additionally, set aggressive stop-loss on trades that have been open
+        # for more than two days
+        for trade in self.trades:
+            if current_time - trade.entry_time > pd.Timedelta('2 days'):
+                if trade.is_long:
+                    trade.sl = max(trade.sl, low)
+                else:
+                    trade.sl = min(trade.sl, high)
+
+bt = Backtest(data, MLTrainOnceStrategy, commission=.0002, margin=.05)
+bt.run()
+bt.plot()
+
+class MLWalkForwardStrategy(MLTrainOnceStrategy):
+    def next(self):
+        # Skip the cold start period with too few values available
+        if len(self.data) < N_TRAIN:
+            return
+
+        # Re-train the model only every 20 iterations.
+        # Since 20 << N_TRAIN, we don't lose much in terms of
+        # "recent training examples", but the speed-up is significant!
+        if len(self.data) % 20:
+            return super().next()
+
+        # Retrain on last N_TRAIN values
+        df = self.data.df[-N_TRAIN:]
+        X, y = get_clean_Xy(df)
+        self.clf.fit(X, y)
+
+        # Now that the model is fitted,
+        # proceed the same as in MLTrainOnceStrategy
+        super().next()
+
+bt2 = Backtest(data, MLWalkForwardStrategy, commission=.0002, margin=.05, trade_on_close=True)
+bt2.run()
+bt2.plot()
